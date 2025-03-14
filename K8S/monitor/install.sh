72a599384a4c79c025859050aab4269f5b1987e2
arcter <arcter01@gmail.com>
2025-03-14 01:31:12 +0100

    working prom

--- K8S/monitor/install.sh
+++ K8S/monitor/install.sh
@@ -1,3 +1,7 @@
 #!/bin/env bash
 
-helm install monitor prometheus-community/kube-prometheus-stack --values monitor-values.yaml --create-namespace --namespace monitor
+
+# Obsolite
+# helm install monitor prometheus-community/kube-prometheus-stack --values monitor-values.yaml --create-namespace --namespace monitor
+
+#source: https://grafana.com/docs/grafana-cloud/monitor-infrastructure/kubernetes-monitoring/configuration/config-other-methods/prometheus/prometheus-operator/
